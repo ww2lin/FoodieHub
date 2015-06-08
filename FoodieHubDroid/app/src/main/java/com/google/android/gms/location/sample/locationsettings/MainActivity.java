8b2968ff766b2758595a2b54f3a0b52cd69a3503
j42lin <j42lin[at]yahoo[dot]com>
2015-06-08 15:05:46 -0400

    Google GPS service in Fragment, Fragment is null
    Needs to be fiexed

---
+++ FoodieHubDroid/app/src/main/java/com/google/android/gms/location/sample/locationsettings/MainActivity.java
@@ -0,0 +1,72 @@
+package com.google.android.gms.location.sample.locationsettings;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import com.google.android.gms.location.sample.locationsettings.UI.GPSService;
+
+
+public class MainActivity extends Activity {
+
+    protected static final String TAG = "yolo";
+
+    //Constant used in the location settings dialog.
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.i(TAG, "Activitiy onCreate");
+        setContentView(R.layout.main_activity);
+        // add Form
+        if (savedInstanceState == null) {
+            // adding a fragment dynamically requires to ensure activity is not recreated from saved instance state
+            // if 'savedInstanceState' is null - this menas activity is created for the first time, ie. app just started
+            getFragmentManager().beginTransaction()
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .add(R.id.rootView, new GPSService(), GPSService.NAME)
+                    .commit();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i(TAG, "Activitiy onStart");
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        Log.i(TAG, "Activitiy onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(TAG, "Activitiy onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, "Activitiy onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            Fragment gpsFragment = (Fragment) getFragmentManager()
+                    .findFragmentById(R.id.gps_wrapper);
+            gpsFragment.onActivityResult(requestCode, resultCode, data);
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
